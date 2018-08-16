74e5a3584b5de283fc829639ed758cc0f7e4f14a
DanhManhNguyen <nguyendanhmanh_t61@hus.edu.vn>
2018-08-16 16:51:44 +0700

    Fill in the timetable

--- app/controllers/class_details_controller.rb
+++ app/controllers/class_details_controller.rb
@@ -1,5 +1,7 @@
 class ClassDetailsController < ApplicationController
   def index
-    crawl_data_from_list
+    # crawl_data_from_list
+    @courses = current_user.courses
+    @class_details = current_user.class_details
   end
 end
